3541dd5d6085ad7473d6218dfbfabf9ba1f43383
Lucas Fernando Macedo Pereira <lucaspereira.lp@dinfor.uniceub>
2026-03-13 21:19:40 -0300

    corrige arquivo eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,19 @@
+const globals = require("globals");
 module.exports = [
-    {
-      files: ["**/*.js"],
-      ignores: ["node_modules/**"],
-      languageOptions: {
-        ecmaVersion: 2021,
-        sourceType: "commonjs"
-      },
-      rules: {
-        "no-unused-vars": "warn",
-        "no-undef": "error"
+  {
+    files: ["**/*.js"],
+    ignores: ["node_modules/**"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.commonjs
       }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error"
     }
-  ];
+  }
+];
